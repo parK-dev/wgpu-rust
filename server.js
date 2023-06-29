0adf59f380a05be1d132d25a372eeb7c42c50693
parK-dev <kirk.paradis@gmail.com>
2023-06-29 23:34:04 +0900

    add server, basic html, and began writing state

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = "." + req.url;
+  if (filePath === "./") {
+    filePath = "./index.html";
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    ".html": "text/html",
+    ".wasm": "application/wasm",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpg",
+    ".gif": "image/gif",
+    ".wav": "audio/wav",
+    ".mp4": "video/mp4",
+    ".woff": "application/font-woff",
+    ".ttf": "application/font-ttf",
+    ".eot": "application/vnd.ms-fontobject",
+    ".otf": "application/font-otf",
+    ".svg": "application/image/svg+xml",
+  };
+
+  const contentType = mimeTypes[extname] || "application/octet-stream";
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      console.log("Error: ", error);
+      res.writeHead(500);
+      res.end(`Error: ${error.code}`);
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
